5100cea08c8e3cec473b4a32f663a200ed62b2c9
RakhaNoor01 <05530@smkrus.sch.id>
2025-09-03 00:00:43 +0700

    feat(ui): styling login

--- lib/widgets/InputField.dart
+++ lib/widgets/InputField.dart
@@ -4,12 +4,14 @@ class InputField extends StatelessWidget {
   final TextEditingController controller;
   final String hint;
   final bool obscureText;
+  final IconData? icon;
 
   const InputField({
     super.key,
     required this.controller,
     required this.hint,
     this.obscureText = false,
+    this.icon,
   });
 
   @override
@@ -19,8 +21,13 @@ class InputField extends StatelessWidget {
       obscureText: obscureText,
       decoration: InputDecoration(
         hintText: hint,
+        prefixIcon: icon != null ? Icon(icon) : null,
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        hintStyle: const TextStyle(color: Colors.white70),
       ),
+      style: const TextStyle(color: Colors.white),
     );
   }
 }
