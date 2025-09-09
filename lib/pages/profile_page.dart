20b5188e494d49d54ede75949201ce90fadae7bc
RakhaNoor01 <05530@smkrus.sch.id>
2025-09-09 08:00:17 +0700

    feat: Profile Styling (Minus Image) - Rakha

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
+import '../widgets/profile_card.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Profile", style: TextStyle(fontSize: 20)),
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      appBar: AppBar(
+        title: const Text(
+          "Our Team Profile",
+          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1.0,
+        iconTheme: const IconThemeData(color: Colors.black87),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+        children: const [
+
+          ProfileCard(
+            nama: "Daffa Bintang Ramadhan",
+            kelas: "XII PPLG 1",
+            absen: "09",
+            gmail: "daffa.bintang@gmail.com",
+            imagePath: "assets/images/daffa.png",
+          ),
+          SizedBox(height: 16),
+          ProfileCard(
+            nama: "Rafan Eka Dinata",
+            kelas: "XII PPLG 1",
+            absen: "30",
+            gmail: "rafaneka@gmail.com",
+            imagePath: "assets/images/raffan.png",
+          ),
+          SizedBox(height: 16),
+          ProfileCard(
+            nama: "Rakha Noor A. Admaja",
+            kelas: "XII PPLG 1",
+            absen: "32",
+            gmail: "rakhanoor12@gmail.com",
+            imagePath: "assets/images/rakhan.png",
+          ),
+        ],
       ),
     );
   }
